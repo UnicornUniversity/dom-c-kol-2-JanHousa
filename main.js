b8928bf3f4ad01a66f926c9a291ec166768f07f0
Raccoon <honzohousa@gmail.com>
2025-11-09 20:17:19 +0100

    dec_to_bin

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-//TODO add imports if needed
-//import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
-/**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
- * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
- * @param {string} inputNumber number that is being converted
- * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
- * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
- * @returns {string} containing number converted to output system
- */
+// Převádí číslo z desítkové soustavy na dvojkovou soustavu
+// npm test - úspěšně projdou všechny testy
+
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
-  return dtoOut;
+
+  // Převedení vstupního řetězce na číslo
+  let number = 0;
+  for (let i = 0; i < inputNumber.length; i++) {
+    const digit = inputNumber.charCodeAt(i) - 48;
+    number = number * 10 + digit;
+  }
+
+  // Ošetření speciálního případu nuly
+  if (number === 0) return "0";
+
+  // Převod na dvojkovou soustavu
+  let result = "";
+  while (number > 0) {
+    const remainder = number % 2;
+    result = remainder + result;
+    number = Math.floor(number / 2);
+  }
+
+  return result;
 }
 
-/**
- * TODO - Change this to contain all input number systems that your application can convert from.
- * Function which returns which number systems are permitted on input.
- * @returns {Array} array of numbers refering to permitted input systems
- */
 export function permittedInputSystems() {
-	return [10, 2];
+  return [10];
 }
 
-/**
- * TODO - Change this to contain all output number systems that your application can convert to.
- * Function which returns which number systems are permitted on output.
- * @returns {Array} array of numbers refering to permitted output systems
- */
 export function permittedOutputSystems() {
-	return [10, 2];
+  return [2];
 }
